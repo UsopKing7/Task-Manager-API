8f2fa445fef21f0c04ad4388364c6e74abcc2e2f
UsopKing7 <programadorusop@gmail.com>
2026-04-18 22:53:25 -0400

    feat: agregamos el modulo de UserInformationModule

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from 'modules/User/infrastructure/user.module'
+import { UserInformationModule } from 'modules/UserInformation/infrastructure/userInformation.module'
 import { PrismaModule } from 'shared/configs/prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule, UserModule]
+  imports: [PrismaModule, UserModule, UserInformationModule]
 })
 export class AppModule {}
